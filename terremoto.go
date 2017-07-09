cb1048152af73a4266666df97262b87e4ecb115b
Marcelo Pinheiro <mpinheir@gmail.com>
2017-07-08 18:41:12 -0700

    Added project information to the start of the terremoto.go file

--- terremoto.go
+++ terremoto.go
@@ -1,3 +1,8 @@
+// Programa em Go para listar os terremotos acima de 6 graus nos últimos 30 dias.
+// Fonte dos dados: https://earthquake.usgs.gov/ - USGS
+// Autor: Marcelo Pinheiro - [Twitter](http://twitter.com/mpinheir)
+//---------------------------------------------------------------------------------------
+
 package main
 
 import (
@@ -5,58 +10,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	//"net/url"
 )
 
 type terremoto struct {
-	Type string `json:"type"`
+	Type     string `json:"type"`
 	Metadata struct {
-		Generated int64 `json:"generated"`
-		URL string `json:"url"`
-		Title string `json:"title"`
-		Status int `json:"status"`
-		API string `json:"api"`
-		Count int `json:"count"`
+		Generated int64  `json:"generated"`
+		URL       string `json:"url"`
+		Title     string `json:"title"`
+		Status    int    `json:"status"`
+		API       string `json:"api"`
+		Count     int    `json:"count"`
 	} `json:"metadata"`
 	Features []struct {
-		Type string `json:"type"`
+		Type       string `json:"type"`
 		Properties struct {
-			Mag float64 `json:"mag"`
-			Place string `json:"place"`
-			Time int64 `json:"time"`
-			Updated int64 `json:"updated"`
-			Tz int `json:"tz"`
-			URL string `json:"url"`
-			Detail string `json:"detail"`
-			Felt int `json:"felt"`
-			Cdi float64 `json:"cdi"`
-			Mmi float64 `json:"mmi"`
-			Alert string `json:"alert"`
-			Status string `json:"status"`
-			Tsunami int `json:"tsunami"`
-			Sig int `json:"sig"`
-			Net string `json:"net"`
-			Code string `json:"code"`
-			Ids string `json:"ids"`
-			Sources string `json:"sources"`
-			Types string `json:"types"`
-			Nst interface{} `json:"nst"`
-			Dmin float64 `json:"dmin"`
-			Rms float64 `json:"rms"`
-			Gap int `json:"gap"`
-			MagType string `json:"magType"`
-			Type string `json:"type"`
-			Title string `json:"title"`
+			Mag     float64     `json:"mag"`
+			Place   string      `json:"place"`
+			Time    int64       `json:"time"`
+			Updated int64       `json:"updated"`
+			Tz      int         `json:"tz"`
+			URL     string      `json:"url"`
+			Detail  string      `json:"detail"`
+			Felt    int         `json:"felt"`
+			Cdi     float64     `json:"cdi"`
+			Mmi     float64     `json:"mmi"`
+			Alert   string      `json:"alert"`
+			Status  string      `json:"status"`
+			Tsunami int         `json:"tsunami"`
+			Sig     int         `json:"sig"`
+			Net     string      `json:"net"`
+			Code    string      `json:"code"`
+			Ids     string      `json:"ids"`
+			Sources string      `json:"sources"`
+			Types   string      `json:"types"`
+			Nst     interface{} `json:"nst"`
+			Dmin    float64     `json:"dmin"`
+			Rms     float64     `json:"rms"`
+			Gap     int         `json:"gap"`
+			MagType string      `json:"magType"`
+			Type    string      `json:"type"`
+			Title   string      `json:"title"`
 		} `json:"properties"`
 		Geometry struct {
-			Type string `json:"type"`
+			Type        string    `json:"type"`
 			Coordinates []float64 `json:"coordinates"`
 		} `json:"geometry"`
 		ID string `json:"id"`
 	} `json:"features"`
 	Bbox []float64 `json:"bbox"`
 }
-
 
 func main() {
 	// JSON URL
@@ -99,19 +102,19 @@ func main() {
 
 	fmt.Println("Terremotos acima de 6 graus na escala Richter, nos últimos 30 dias:")
 	fmt.Println("------------------------------------------------")
-	
+
 	var i int
 
 	for i = 0; i < record.Metadata.Count; i++ {
 
-			j := record.Features[i].Properties.Mag
+		j := record.Features[i].Properties.Mag
 
-			if ( j > 6){
-				fmt.Println("Epicentro = ", record.Features[i].Properties.Place)
-				fmt.Println("Magnitude: ", record.Features[i].Properties.Mag)
-				fmt.Println("------------------------------------------------")
-				//fmt.Println("Fuso horário: ", record.Features[i].Properties.Tz)
-				//fmt.Println("Horário: ", record.Features[i].Properties.Time)
-			}
+		if j > 6 {
+			fmt.Println("Epicentro = ", record.Features[i].Properties.Place)
+			fmt.Println("Magnitude: ", record.Features[i].Properties.Mag)
+			fmt.Println("------------------------------------------------")
+			//fmt.Println("Fuso horário: ", record.Features[i].Properties.Tz)
+			//fmt.Println("Horário: ", record.Features[i].Properties.Time)
+		}
 	}
 }
